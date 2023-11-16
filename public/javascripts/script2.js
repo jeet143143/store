eaa862a1456ef18027d291553bb3e2f3cb44110f
jeet143143 <senapatijeet2004@gmail.com>
2023-11-16 12:35:19 +0530

    loading page

---
+++ public/javascripts/script2.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+	const loader = document.querySelector(".loader");
+	loader.style.opacity = "0";
+	loader.style.backgroundColor = "transparent";
+	setTimeout(() => {
+		loader.style.display = "none";
+		document.body.style.overflow = "visible";
+	}, 1000);
+}, 3000);
